46d8321d4c484ee4d90c467976d1a2c37ccc0b2e
Baiyu4 <3286755903@qq.com>
2020-11-11 08:39:20 +0800

    创建路由

--- them/src/router/index.js
+++ them/src/router/index.js
@@ -3,7 +3,30 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [{
+  path:"/",
+  redirect:"/home",
+},{
+  path:"/home",
+  name:"Home",
+  component:()=>import("../views/Home/index.vue")
+},{
+  path:"/them",
+  name:"Them",
+  component:()=>import("../views/Them/index.vue")
+},{
+  path:"/search",
+  name:"Search",
+  component:()=>import("../views/Search/index.vue")
+},{
+  path:"/shop",
+  name:"Shop",
+  component:()=>import("../views/Shop/index.vue")
+},{
+  path:"/my",
+  name:"My",
+  component:()=>import("../views/My/index.vue")
+}]
 
 const router = new VueRouter({
   mode: 'history',
